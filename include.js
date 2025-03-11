5a0e79a9c317277f511a7eb60e2f90c0784e70ac
Lukaserikss0n <lukas.emil.eriksson@outlook.com>
2025-03-11 21:03:20 +0100

    navbar uppdates: cosmatic, gradient on logo added, active side displayd in navbar, media-600px hamburger menu started on

--- include.js
+++ include.js
@@ -1,6 +1,21 @@
-fetch('header.html')
-.then(response => response.text())
-.then(data => {
-  document.getElementById('header').innerHTML = data;
-})
-.catch(error => console.error('Error loading header:', error));
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("header.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("header").innerHTML = data;
+
+          highlightActiveLink();
+      });
+
+  function highlightActiveLink() {
+      let currentLocation = window.location.pathname.split("/").pop();
+
+      let navLinks = document.querySelectorAll("nav ul li a");
+
+      navLinks.forEach(link => {
+          if (link.getAttribute("href") === currentLocation || (currentLocation === "" && link.getAttribute("href") === "index.html")) {
+              link.classList.add("active");
+          }
+      });
+  }
+});
